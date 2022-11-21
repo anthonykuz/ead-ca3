35b6c3ac8c5ec0180191cc111757e7ae99cf274a
anthonykuz <x00162229@mytudublin.ie>
2022-11-21 23:01:11 +0000

    updated styling, added collapsable fact cards, moved css into seperate file, added js event listeners

---
+++ ca3_x00162229/ca3_x00162229/wwwroot/js/script.js
@@ -0,0 +1,23 @@
+﻿
+document.querySelector('body').addEventListener('click', function (event) {
+    
+});
+
+let touchFact = function (event) {
+    if (event.target.className === 'fact-collapsed') {
+        event.target.classList.remove('fact-collapsed');
+        event.target.classList.add('fact');
+
+    } else if (event.target.className === 'fact') {
+        event.target.classList.add('fact-collapsed');
+        event.target.classList.remove('fact');
+    }
+
+    else if (event.target.parentElement.className === 'fact-collapsed') {
+        event.target.parentElement.classList.remove('fact-collapsed');
+        event.target.parentElement.classList.add('fact');
+    } else if (event.target.parentElement.className === 'fact') {
+        event.target.parentElement.classList.add('fact-collapsed');
+        event.target.parentElement.classList.remove('fact');
+    }
+}
